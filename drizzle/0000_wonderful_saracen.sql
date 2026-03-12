ea95d2c0d33cac61ce3da3b5ac15c001855de06d
Robert Walter <robertwalter2003@proton.me>
2026-03-12 15:34:38 +0100

    Set up Neon Postgres with Drizzle

---
+++ drizzle/0000_wonderful_saracen.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "user" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"role" varchar(50) DEFAULT 'user' NOT NULL,
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp NOT NULL,
+	"updated_from" varchar(255) DEFAULT 'unknown' NOT NULL,
+	CONSTRAINT "user_email_unique" UNIQUE("email")
+);
